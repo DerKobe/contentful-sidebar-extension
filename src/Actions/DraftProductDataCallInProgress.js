0579e0a6bd2f4a9b292d561fef3699f194760a26
Philip Claren <philip.claren@googlemail.com>
2019-12-10 16:30:40 +0100

    .

--- src/Actions/DraftProductDataCallInProgress.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import EnrichButton from './shared/EnrichButton'
-
-export function DraftProductDataCallInProgress() {
-  return (
-    <EnrichButton buttonType="naked" apiCallInProgressOverride />
-  )
-}
